4e9d2c427102946ffafb395c0c15e3ced7e16caa
mariavnn <marianieto290520@gmail.com>
2023-10-04 16:15:38 -0500

    Prueba Deploy Vercel

--- api/index.js
+++ api/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 })
+
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () =>{
     console.log(`App is running on http://localhost:${port}`)
